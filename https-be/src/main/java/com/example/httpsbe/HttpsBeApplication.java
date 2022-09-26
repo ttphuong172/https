9ef51d8d6b62f7561820c0fbc9ee82e71531638a
Tran Thanh Phuong <ttphuong172@gmail.com>
2022-09-26 19:45:23 +0700

    fisrt

---
+++ https-be/src/main/java/com/example/httpsbe/HttpsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsBeApplication.class, args);
+	}
+
+}
---
+++ https-be/src/test/java/com/example/httpsbe/HttpsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
